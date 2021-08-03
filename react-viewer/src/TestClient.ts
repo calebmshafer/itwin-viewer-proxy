50c60b5bb60b16457a047305db919b45bdf2bf32
Arun George <aruniverse@users.noreply.github.com>
2021-08-03 09:50:19 -0400

    update viewer

--- react-viewer/src/TestClient.ts
+++ react-viewer/src/TestClient.ts
@@ -1,41 +1,46 @@
-import { AuthStatus, BeEvent, BentleyError, ClientRequestContext } from "@bentley/bentleyjs-core";
+import {
+  AuthStatus,
+  BeEvent,
+  BentleyError,
+  ClientRequestContext,
+} from "@bentley/bentleyjs-core";
 import { AccessToken } from "@bentley/itwin-client";
 import { FrontendAuthorizationClient } from "@bentley/frontend-authorization-client";
 
 /** This client defines the interfaces  */
 export class NoSignInIAuthClient implements FrontendAuthorizationClient {
-  public readonly onUserStateChanged: BeEvent<(token: AccessToken | undefined) => void>;
+  public readonly onUserStateChanged: BeEvent<
+    (token: AccessToken | undefined) => void
+  >;
   protected _accessToken?: AccessToken;
 
   private static _oidcClient: FrontendAuthorizationClient;
 
   public static get oidcClient(): FrontendAuthorizationClient {
-    if (undefined !== this._oidcClient)
+    if (this._oidcClient) {
       return this._oidcClient;
+    }
     this._oidcClient = new NoSignInIAuthClient();
     return this._oidcClient;
   }
 
   constructor() {
     this.onUserStateChanged = new BeEvent();
-    this.generateTokenString();
+    this.generateToken();
   }
 
   public async signIn(requestContext?: ClientRequestContext): Promise<void> {
-    if (requestContext) {
-      requestContext.enter();
-    }
-    await this.getAccessToken();
+    requestContext?.enter();
+    this.generateToken();
   }
-  public async signOut(requestContext?: ClientRequestContext): Promise<void> {
-    if (requestContext)
-      requestContext.enter();
 
+  public async signOut(requestContext?: ClientRequestContext): Promise<void> {
+    requestContext?.enter();
     this._accessToken = undefined;
   }
 
   public get isAuthorized(): boolean {
-    return true;
+    return !!this._accessToken;
   }
 
   public get hasExpired(): boolean {
@@ -43,22 +48,22 @@ export class NoSignInIAuthClient implements FrontendAuthorizationClient {
   }
 
   public get hasSignedIn(): boolean {
-    return true;
+    return !!this._accessToken;
   }
 
-  public async generateTokenString() {
-    const tokenJson = {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      userInfo: { id: "MockId" },
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      tokenString: "Bearer t",
-    };
-    this._accessToken = AccessToken.fromJson(tokenJson);
+  public generateToken() {
+    if (!this._accessToken) {
+      this._accessToken = AccessToken.fromJson({
+        userInfo: { id: "MockId" },
+        tokenString: "Bearer t",
+      });
+    }
   }
 
   public async getAccessToken(): Promise<AccessToken> {
-    if (!this._accessToken)
+    if (!this._accessToken) {
       throw new BentleyError(AuthStatus.Error, "Cannot get access token");
+    }
     return this._accessToken;
   }
 }
